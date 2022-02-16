1fc80e684a1b2ac8d0d11b42c6749a967bb52b3d
Nav Roudsari <nav@roudsari.io>
2022-02-16 15:09:02 +0000

    [test] - add pyecc test vectors

--- test/chia_bls_dart_test.dart
+++ test/chia_bls_dart_test.dart
@@ -536,4 +536,56 @@ void main() {
               "8227c6c716b1e15791459850f2289168fa0cf9115"));
     });
   });
+
+  group('IETF test vectors', () {
+    test('Pyecc vector', () {
+      var refSig1Basic =
+          '96ba34fac33c7f129d602a0bc8a3d43f9abc014eceaab7359146b4b150e57b808645738f35671e9e10e0d862a30cab70074eb5831d13e6a5b162d01eebe687d0164adbd0a864370a7c222a2768d7704da254f1bf1823665bc2361f9dd8c00e99';
+      var refSig2Basic =
+          'a402790932130f766af11ba716536683d8c4cfa51947e4f9081fedd692d6dc0cac5b904bee5ea6e25569e36d7be4ca59069a96e34b7f700758b716f9494aaa59a96e74d14a3b552a9a6bc129e717195b9d6006fd6d5cef4768c022e0f7316abf';
+      var refSigABasic =
+          '987cfd3bcd62280287027483f29c55245ed831f51dd6bd999a6ff1a1f1f1f0b647778b0167359c71505558a76e158e66181ee5125905a642246b01e7fa5ee53d68a4fe9bfb29a8e26601f0b9ad577ddd18876a73317c216ea61f430414ec51c5';
+      var refSig1Aug =
+          '8180f02ccb72e922b152fcedbe0e1d195210354f70703658e8e08cbebf11d4970eab6ac3ccf715f3fb876df9a9797abd0c1af61aaeadc92c2cfe5c0a56c146cc8c3f7151a073cf5f16df38246724c4aed73ff30ef5daa6aacaed1a26ecaa336b';
+      var refSig2Aug =
+          '99111eeafb412da61e4c37d3e806c6fd6ac9f3870e54da9222ba4e494822c5b7656731fa7a645934d04b559e9261b86201bbee57055250a459a2da10e51f9c1a6941297ffc5d970a557236d0bdeb7cf8ff18800b08633871a0f0a7ea42f47480';
+      var refSigAAug =
+          '8c5d03f9dae77e19a5945a06a214836edb8e03b851525d84b9de6440e68fc0ca7303eeed390d863c9b55a8cf6d59140a01b58847881eb5af67734d44b2555646c6616c39ab88d253299acc1eb1b19ddb9bfcbe76e28addf671d116c052bb1847';
+      var refSig1Pop =
+          '9550fb4e7f7e8cc4a90be8560ab5a798b0b23000b6a54a2117520210f986f3f281b376f259c0b78062d1eb3192b3d9bb049f59ecc1b03a7049eb665e0df36494ae4cb5f1136ccaeefc9958cb30c3333d3d43f07148c386299a7b1bfc0dc5cf7c';
+      var refSig2Pop =
+          'a69036bc11ae5efcbf6180afe39addde7e27731ec40257bfdc3c37f17b8df68306a34ebd10e9e32a35253750df5c87c2142f8207e8d5654712b4e554f585fb6846ff3804e429a9f8a1b4c56b75d0869ed67580d789870babe2c7c8a9d51e7b2a';
+      var refSigAPop =
+          'a4ea742bcdc1553e9ca4e560be7e5e6c6efa6a64dddf9ca3bb2854233d85a6aac1b76ec7d103db4e33148b82af9923db05934a6ece9a7101cd8a9d47ce27978056b0f5900021818c45698afdd6cf8a6b6f7fee1f0b43716f55e413d4b87a6039';
+
+      var secret1 = Uint8List.fromList(List.filled(32, 0x01));
+      var secret2 =
+          Uint8List.fromList(List.generate(32, (x) => x * 314159 % 256));
+
+      var sk1 = PrivateKey.fromBytes(secret1);
+      var sk2 = PrivateKey.fromBytes(secret2);
+
+      var msg = Uint8List.fromList(([3, 1, 4, 1, 5, 9]));
+      var sig1Basic = BasicSchemeMPL().sign(sk1, msg);
+      var sig2Basic = BasicSchemeMPL().sign(sk2, msg);
+      var sigABasic =
+          BasicSchemeMPL().aggregateSignatures([sig1Basic, sig2Basic]);
+      var sig1Aug = AugSchemeMPL().sign(sk1, msg);
+      var sig2Aug = AugSchemeMPL().sign(sk2, msg);
+      var sigAAug = AugSchemeMPL().aggregateSignatures([sig1Aug, sig2Aug]);
+      var sig1Pop = PopSchemeMPL().sign(sk1, msg);
+      var sig2Pop = PopSchemeMPL().sign(sk2, msg);
+      var sigAPop = PopSchemeMPL().aggregateSignatures([sig1Pop, sig2Pop]);
+
+      expect(sig1Basic.toBytes().toHexString(), equals(refSig1Basic));
+      expect(sig2Basic.toBytes().toHexString(), equals(refSig2Basic));
+      expect(sigABasic.toBytes().toHexString(), equals(refSigABasic));
+      expect(sig1Aug.toBytes().toHexString(), equals(refSig1Aug));
+      expect(sig2Aug.toBytes().toHexString(), equals(refSig2Aug));
+      expect(sigAAug.toBytes().toHexString(), equals(refSigAAug));
+      expect(sig1Pop.toBytes().toHexString(), equals(refSig1Pop));
+      expect(sig2Pop.toBytes().toHexString(), equals(refSig2Pop));
+      expect(sigAPop.toBytes().toHexString(), equals(refSigAPop));
+    });
+  });
 }
